604ccdbf366c6b0cf11d68f79adfe27308fbfe89
冯立强 <1065846339@qq.com>
2026-06-05 16:47:41 +0800

    feat:封装axios

---
+++ src/service/request.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+const service = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    timeout: 5000,
+})
+
+//请求拦截器
+service.interceptors.request.use(config => {
+    //从本地拿取
+    const token = ''
+    if (token) {
+        config.headers.Authorization = token
+    }
+    return config
+})
+
+
+//响应拦截器
+service.interceptors.response.use(response => {
+    const res = response.data
+    if (res.code === 200) {
+        return response
+    }
+    return Promise.reject(res)
+
+})
+const http = {
+    get(url: string, params: object, config = {}) {
+        return service.get(url, {
+            ...config,
+            params,
+        })
+    },
+    post(url: string, data: object, config = {}) {
+        return service.post(url, data, config)
+    },
+    put(url: string, data: object, config = {}) {
+        return service.put(url, data, config)
+    },
+    delete(url: string, data: object, config = {}) {
+        return service.delete(url, {
+            ...config,
+            data,
+        })
+    }
+}
+export default http
